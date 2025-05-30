8952ff6e9ae8bc846a975b3f846e07320737f6da
Rainers Lovkins <rainerslovkins@gmail.com>
2025-05-30 16:44:36 +0300

    fix: update Stripe API version in webhook route
    - Changed the Stripe API version from '2023-10-16' to '2024-11-20.acacia' in the webhook route file to ensure compatibility with the latest features and improvements.

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -4,7 +4,7 @@ import { updateOrderToPaid } from '@/lib/actions/order.actions';
 
 // Initialize Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2024-11-20.acacia',
 });
 
 export async function POST(req: NextRequest) {
